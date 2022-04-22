e3b42e611d022c4dd44c67a37a2d7e2c6efc6cc5
boonej <jeboone@gmail.com>
2022-04-22 10:10:01 -0500

    updated version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Jonathan Boone'
 author = 'Jonathan Boone'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
